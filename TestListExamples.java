e5f3b9c66113efa381fcd24a459d702bcede981a
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-10 11:34:16 -0800

    change

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter(){
+      ArrayList<String> input =  new ArrayList<String>();
+      input.add("a");
+      input.add("b");
+      input.add("longword");
+      ArrayList<String>output =input;
+      output.remove("longword");
+      StringChecker sc = new shortWord();
+
+      assertArrayEquals(output.toArray(),ListExamples.filter(input,sc).toArray());
+  }
+
 }
