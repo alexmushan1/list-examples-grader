91146b81bad40bfc714b2594a368d23add838e29
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-10 11:44:37 -0800

    added -R

--- grade.sh
+++ grade.sh
@@ -7,7 +7,7 @@ git clone $1 student-submission
 
 CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 cp TestListExamples.java student-submission/
-cp lib student-submission
+cp -R lib student-submission
 cd student-submission
 if ! [[ -e ListExamples.java ]]
 then 
