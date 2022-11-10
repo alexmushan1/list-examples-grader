0e4f97ed3c79a39d2d8ab442fbd7e1223b90333e
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-10 11:42:28 -0800

    edit

--- grade.sh
+++ grade.sh
@@ -5,16 +5,17 @@
 rm -rf student-submission
 git clone $1 student-submission
 
-CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 cp TestListExamples.java student-submission/
+cp lib student-submission
 cd student-submission
-if ![[-e ListExamples.java]]
+if ! [[ -e ListExamples.java ]]
 then 
     echo "wrong file provided"
     exit 1
 else
     javac -cp $CP*.java 2> compileErr.txt
-    if ![[$? -eq 0]]
+    if ! [[ $? -eq 0 ]]
     then
         echo compileErr.txt $?
         exit 1
