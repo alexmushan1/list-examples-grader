03972064fd36327a0ad6b042b770e10416ea3def
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-27 14:56:37 -0800

    tests edit

--- TestListExamples.java
+++ TestListExamples.java
@@ -8,15 +8,23 @@ public class TestListExamples {
   // Write your grading tests here!
   @Test
   public void testFilter(){
-      ArrayList<String> input =  new ArrayList<String>();
-      input.add("a");
-      input.add("b");
-      input.add("longword");
-      ArrayList<String>output =input;
-      output.remove("longword");
-      StringChecker sc = new shortWord();
+    StringChecker sc = (String s) -> !s.isEmpty();
+    ArrayList<String> input =  new ArrayList<String>();
+    input.add("a");
+    input.add("b");
+    input.add("");
+    ArrayList<String>output =input;
+    Assert.assertEquals(output, ListExamples.filter(input, sc));
+    output.remove("");
+    //assertArrayEquals(output.toArray(),ListExamples.filter(input,sc).toArray());
+  }
 
-      assertArrayEquals(output.toArray(),ListExamples.filter(input,sc).toArray());
+  @Test
+  public void testMerge(){
+    ArrayList<String> input1 = new ArrayList<>(Arrays.asList("a"));
+    ArrayList<String> input2 = new ArrayList<>(Arrays.asList("b", "c", "d"));
+    ArrayList<String> output = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+    Assert.assertEquals(output, ListExamples.merge(input1, input2));
   }
 
 }
