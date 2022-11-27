7ab40ee91accf3457d4c6468d1d51d2a0011f1c1
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-27 15:46:21 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,51 +1,31 @@
-rm -rf stderr.txt
+set -e
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
-cd student-submission
-if  ! [ -e ListExamples.java ]
-
-	then
-		echo "ListExamples.java file not found"
-		exit 1
-
-	else
-		cp ListExamples.java ./../
-		cd ..
-		javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> stderr.txt
+# check if the correct file submitted
+cd student-submission/
+if [ -e ListExamples.java ]
+then 
+echo "correct file submitted"
+else 
+echo "please submit the correct file"
+exit 
 fi
 
-echo
+# copy the test to the same folder as student's submissions
+cd ../
+cp TestListExamples.java student-submission/
+cd student-submission/
 
-[ -s stderr.txt ]
-
-if [ $? -eq 0 ]
-
+# compile student code
+set +e
+javac -cp $CPATH *.java
+# notification if compile failed
+if [$? -ne 0]
 then
-	echo "ListExamples.java file can't compile!"
-	exit 1
+echo "compile failed"
+set -e
 
-else
-	echo "ListExamples.java file is compiled successfully!"
-fi
-
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > stdout.txt
-
-echo
-
-COUNT_FILTER_TEST=$(grep -o "ListExamples.filter(" TestListExamples.java | wc -l)
-
-COUNT_MERGE_TEST=$(grep -o "ListExamples.merge(" TestListExamples.java | wc -l)
-
-COUNT_FILTER=$(grep -o "testFilter" stdout.txt | wc -l)
-
-COUNT_MERGE=$(grep -o "testMerge" stdout.txt | wc -l)
-
-PASSED_FILTER=$(echo "$COUNT_FILTER_TEST-$COUNT_FILTER/2" | bc)
-
-PASSED_MERGE=$(echo "$COUNT_MERGE_TEST-$COUNT_MERGE/2" | bc)
-
-echo "you passed $PASSED_FILTER out of $COUNT_FILTER_TEST test for filter() method!"
-
-echo
-
-echo "you passed $PASSED_MERGE out of $COUNT_MERGE_TEST for merge() method!"
+# run the tests
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt 2> compileErr.txt 
+cat output.txt err-output.txt
