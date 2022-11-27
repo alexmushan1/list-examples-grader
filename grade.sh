b666e291ad5332c6dce1531b2eddda8dc257b530
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-11-27 15:15:17 -0800

    a

--- grade.sh
+++ grade.sh
@@ -18,7 +18,7 @@ else
     #cp TestListExamples.java student-submission/
     cp ListExamples.java ./../
     #cp -R lib student-submission
-    cd ..
+    #cd ..
     javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> compileErr.txt
     #javac -cp $CP*.java 2> compileErr.txt
 fi
